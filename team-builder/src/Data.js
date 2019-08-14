140235bc078609ff80d118e5a3efc431374e4222
Carl-Stanley <cds.beml@gmail.com>
2019-08-13 21:06:04 -0700

    Commit

--- team-builder/src/Data.js
+++ team-builder/src/Data.js
@@ -1,9 +1,17 @@
 const teamMembers = {
     
     name : "John Lennon",
+    email : "jlennone@beatles.org",
+    role : "Backup Vocals/Guitar",
     name : "Paul McCartney",
+    email : "pmcartney@beatles.org",
+    role : "Front Man",
     name : "Ringo Star",
-    name : "George Harrison"
+    email : "rstar@beatles.org",
+    role : "Drummer",
+    name : "George Harrison",
+    email : "gharrison@beatles.org",
+    role : "Bass/Guitar"
 };
 
 export default teamMembers;
